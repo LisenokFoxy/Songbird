9c16e4303ab68eb15d68653aa981d636f89165d6
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-07-28 22:25:35 +0600

    init: start songbird task

---
+++ songbird/webpack.config.js
@@ -0,0 +1,50 @@
+const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.svg$/,
+        loader: "svg-inline-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+
+  // devServer: {
+  //   port: 4200,
+  // },
+};
+
+module.exports = config;
