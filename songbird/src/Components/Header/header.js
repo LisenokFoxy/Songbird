387181ba406ce33c788f12b7d8d373ce95b6a16c
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-08-09 14:39:51 +0600

    fix: correct header

--- songbird/src/Components/Header/header.js
+++ songbird/src/Components/Header/header.js
@@ -1,20 +1,33 @@
-import React from "react";
-import { Section } from "./section";
+// import React, { Component } from './node_modules/react'
+import React from 'react';
+import './style.css';
 
-export class Header extends React.Component {
+class Header extends React.Component {
   render() {
-    const score = this.props.score;
-
     return (
-      <div>
-        <div className="header-logo">
-          <div className="logo"></div>
-          <div className="score">
-            Score: <span className="span-score">{score}</span>
+      <header>
+        <div className="score">
+          <div className="logo">
+            <h1>Musical Instruments</h1>
           </div>
+          <p className="score">Score: {this.props.score}</p>
         </div>
-        <Section></Section>
-      </div>
+        <QuestionsList />
+      </header>
     );
   }
 }
+
+class QuestionsList extends React.Component {
+  render() {
+    const questionArr = ['Разминка', 'Медные духовые', 'Ударные', 'Струнные щипковые', 'Струнные смычковые', 'Идиофоны'];
+    const questionItems = questionArr.map((question, index) =>
+      <li className={index ? "question-item" : "question-item active"} key={index}>{question}</li>
+    );
+    return (
+      <ul className="question-items">{questionItems}</ul>
+    )
+  }
+}
+
+export default Header
