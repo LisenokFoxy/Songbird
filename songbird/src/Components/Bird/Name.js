eaeb767dcc12582ddfc200871e62230680971cf0
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-08-09 14:39:04 +0600

    refactor: delete unused files

--- songbird/src/Components/Bird/Name.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export class Name extends React.Component {
-  render() {
-    const name = this.props.name;
-    return <div className="name-bird">{name}</div>;
-  }
-}
--- songbird/src/Components/Info/Info.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-export class Info extends React.Component {
-  render() {
-    const info = this.props.info;
-    return <div className="info">{info}</div>;
-  }
-}
