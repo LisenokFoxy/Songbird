01c21060be713f6dfcd7f1191546a18953b17df8
Tatyana Pashintseva <Tatyana.Pashintseva@kcell.kz>
2020-08-13 20:47:36 +0600

    fix: modify app files & styles

--- songbird/src/App.js
+++ songbird/src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "./App.css";
-import { Header } from "./Components/Header/header";
+// import React from "react";
+// import "./App.css";
+// import { Header } from "./components/Header/Header";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="wrapper">{<Header></Header>}</div>
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <div className="wrapper">{<Header></Header>}</div>
+//     </div>
+//   );
+// }
 
-export default App;
+// export default App;
